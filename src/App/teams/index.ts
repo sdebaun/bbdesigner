2090c27b2a6701a93c0b7faff127934cc4aaadbe
Stephen DeBaun <sdebaun74@gmail.com>
2021-08-23 01:26:30 -0700

    moving around

---
+++ src/App/teams/index.ts
@@ -0,0 +1,34 @@
+import { TeamType } from '../models/TeamType'
+
+import { ChaosDwarf } from './ChaosDwarf'
+import { HighElf } from './HighElf'
+import { Vampire } from './Vampire'
+import { Brettonian } from './Brettonian'
+import { Undead } from './Undead'
+import { Necromantic } from './Necromantic'
+import { Khemri } from './Khemri'
+import { AfterlifeUnited } from './AfterlifeUnited'
+import { SuperiorBeingRing } from './SuperiorBeingRing'
+
+export type TeamTypeKey =
+    | 'ChaosDwarf'
+    | 'HighElf'
+    | 'Vampire'
+    | 'Brettonian'
+    | 'SuperiorBeingRing'
+    | 'Undead'
+    | 'Necromantic'
+    | 'Khemri'
+    | 'AfterlifeUnited'
+
+export const TeamTypes: {[k in TeamTypeKey]: TeamType} = {
+    ChaosDwarf,
+    HighElf,
+    Vampire,
+    Brettonian,
+    SuperiorBeingRing,
+    Undead,
+    Necromantic,
+    Khemri,
+    AfterlifeUnited,
+}
