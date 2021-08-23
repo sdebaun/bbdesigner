d026fc778456ffe90fbd489c6b0eae9ee9a42f7b
Stephen DeBaun <sdebaun74@gmail.com>
2021-08-23 04:00:58 -0700

    clean

--- src/App/teams/ChaoticPlayerPact.ts
+++ src/App/teams/ChaoticPlayerPact.ts
@@ -1,10 +1,5 @@
 import { BASE_UPGRADE_COSTS, TeamType } from "../models";
 
-import { Undead } from './Undead'
-import { NecromanticFleshGolem, NecromanticWerewolf } from './Necromantic'
-import { KhemriThrower, KhemriBlitzer, KhemriTombGuardian } from './Khemri'
-import { Vampire } from './Vampire'
-
 import { Chaos } from './Chaos'
 import { Skaven } from './Skaven'
 import { DarkElf } from "./DarkElf"
--- src/App/teams/Underworld.ts
+++ src/App/teams/Underworld.ts
@@ -1,13 +1,6 @@
 import { BASE_UPGRADE_COSTS, TeamType } from "../models";
 
-import { Undead } from './Undead'
-import { NecromanticFleshGolem, NecromanticWerewolf } from './Necromantic'
-import { KhemriThrower, KhemriBlitzer, KhemriTombGuardian } from './Khemri'
-import { Vampire } from './Vampire'
-
-import { Chaos } from './Chaos'
 import { Skaven } from './Skaven'
-import { DarkElf } from "./DarkElf"
 import { Goblin } from "./Goblin"
 
 export const Underworld: TeamType = {
