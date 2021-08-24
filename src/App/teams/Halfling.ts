020c9b8e95d3848abc097fefb0d34e92c8937ab7
Stephen DeBaun <sdebaun74@gmail.com>
2021-08-24 16:15:14 -0700

    fix lings

--- src/App/teams/Halfling.ts
+++ src/App/teams/Halfling.ts
@@ -14,26 +14,6 @@ export const HalflingLineman: Position = prepend({
     max: 16,
 })
 
-export const HalflingCatcher: Position = prepend({
-    title: 'Catcher',
-    ma: 5, st: 2, ag: 3, av: 6,
-    startingSkills: ['Catch', 'Dodge', 'Right Stuff', 'Sprint', 'Stunty'],
-    normal: Normal.A,
-    double: Double.GSP,
-    cost: 50,
-    max: 2,
-})
-
-export const HalflingHefty: Position = prepend({
-    title: 'Hefty',
-    ma: 5, st: 2, ag: 3, av: 7,
-    startingSkills: ['Dodge', 'Fend', 'Stunty'],
-    normal: Normal.AP,
-    double: Double.GS,
-    cost: 50,
-    max: 2,
-})
-
 export const HalflingTreeman: Position = prepend({
     title: 'Treeman',
     ma: 4, st: 5, ag: 1, av: 9,
@@ -49,8 +29,6 @@ export const Halfling: TeamType = {
     upgradeCosts: {'Team Reroll': 60, ...BASE_UPGRADE_COSTS},
     positions: [
         HalflingLineman,
-        HalflingCatcher,
-        HalflingHefty,
         HalflingTreeman,
     ]
 }
